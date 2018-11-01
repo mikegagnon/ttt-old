71fde8fe3b7b299fd17ab07f3225b068ef9cf4df
Michael Gagnon <mikegagnon@gmail.com>
2018-11-01 12:17:00 -0700

    Check for victory

--- ttt-step04/js/ttt-step04.js
+++ ttt-step04/js/ttt-step04.js
@@ -2,9 +2,11 @@ TURN = 1;
 
 var PLAYED_CELL = new Array(9);
 
+var GAME_OVER = false;
+
 function cellClick(cellNumber) {
 
-    if (PLAYED_CELL[cellNumber] == 1 || PLAYED_CELL[cellNumber] == 2) {
+    if (GAME_OVER || PLAYED_CELL[cellNumber] == 1 || PLAYED_CELL[cellNumber] == 2) {
         return;
     }
 
@@ -12,6 +14,17 @@ function cellClick(cellNumber) {
     var imgTag = getImgTag(TURN);
     $(cellId).append(imgTag);
     PLAYED_CELL[cellNumber] = TURN;
+
+    GAME_OVER = checkForVictory(1) || checkForVictory(2);
+
+    if (GAME_OVER) {
+        if (TURN == 1) {
+            alert("Player X wins!");
+        } else {
+            alert("Player O wins!");
+        }
+    }
+
     if (TURN == 1) {
         TURN = 2;
     } else {
@@ -27,4 +40,15 @@ function getImgTag(player) {
         filename = "img/player-o.png";
     }
     return "<img src='" + filename + "'>";
+}
+
+function checkForVictory(player) {
+    return (PLAYED_CELL[0] == player && PLAYED_CELL[1] == player && PLAYED_CELL[2] == player) ||
+        (PLAYED_CELL[3] == player && PLAYED_CELL[4] == player && PLAYED_CELL[5] == player) ||
+        (PLAYED_CELL[6] == player && PLAYED_CELL[7] == player && PLAYED_CELL[8] == player) ||
+        (PLAYED_CELL[0] == player && PLAYED_CELL[3] == player && PLAYED_CELL[6] == player) ||
+        (PLAYED_CELL[1] == player && PLAYED_CELL[4] == player && PLAYED_CELL[7] == player) ||
+        (PLAYED_CELL[2] == player && PLAYED_CELL[5] == player && PLAYED_CELL[8] == player) ||
+        (PLAYED_CELL[0] == player && PLAYED_CELL[4] == player && PLAYED_CELL[8] == player) ||
+        (PLAYED_CELL[6] == player && PLAYED_CELL[4] == player && PLAYED_CELL[2] == player);
 }
