28de3b1e9b57ad21fcaa48992f7c34133af3ef5e
Michael Gagnon <mikegagnon@gmail.com>
2018-11-03 21:00:42 -0700

    Update step1 with vertical-centering

--- ttt-step01/js/ttt-step01.js
+++ ttt-step01/js/ttt-step01.js
@@ -2,10 +2,10 @@ TURN = 1;
 
 function cellClick(cellNumber) {
     if (TURN == 1) {
-        $(".cell").eq(cellNumber).text("x");
+        $(".cell").eq(cellNumber).text("X");
         TURN = 2;
     } else {
-        $(".cell").eq(cellNumber).text("o");
+        $(".cell").eq(cellNumber).text("O");
         TURN = 1;
     }
 }
