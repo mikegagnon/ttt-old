29345c3a65c07458ca5a3f90a2b8d0e704a935ab
Michael Gagnon <mikegagnon@gmail.com>
2018-11-04 20:39:20 -0800

    simplify

--- ttt-step00/js/ttt-step00.js
+++ ttt-step00/js/ttt-step00.js
@@ -1,7 +1,8 @@
 
 function cellClick() {
-    var cellNumber = $(this).data("cell-number");
-    $(".cell").eq(cellNumber).text("X");
+    //var cellNumber = $(this).data("cell-number");
+    // /$(".cell").eq(cellNumber).text("X");
+    $(this).text("X");
 }
 
 $(".cell").click(cellClick);
